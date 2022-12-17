a6aa13a9b78e4af8d0376b65fb11191b1b4a09d0
Soh Mei Ling <meigemini.91@gmail.com>
2022-12-17 11:24:05 +0800

    update code

--- ping-pong.py
+++ ping-pong.py
@@ -5,10 +5,7 @@ from pygame import *
 class GameSprite(sprite.Sprite):
     #class constructor
     def __init__(self, player_image, player_x, player_y, size_x, size_y, player_speed):
-        #Call for the class (Sprite) constructor:
         super().__init__() #sprite.Sprite.__init__(self)
- 
-        #every sprite must store the image property
         self.image = transform.scale(image.load(player_image), (size_x, size_y))
         self.speed = player_speed
  
@@ -26,15 +23,15 @@ class Paddle (GameSprite):
     #method to control the sprite with arrow keys
     def update_right(self):
         keys = key.get_pressed()
-        if keys[K_LEFT] and self.rect.x > 5:
+        if keys[K_UP] and self.rect.y > 5:
             self.rect.y -= self.speed
-        if keys[K_RIGHT] and self.rect.x < win_width - 80:
+        if keys[K_DOWN] and self.rect.y < win_width - 80:
             self.rect.y += self.speed
     def update_left(self):
         keys = key.get_pressed()
-        if keys[K_w] and self.rect.x > 5:
+        if keys[K_w] and self.rect.y > 5:
             self.rect.y -= self.speed
-        if keys[K_s] and self.rect.x < win_width - 80:
+        if keys[K_s] and self.rect.y < win_width - 80:
             self.rect.y += self.speed
 
 #interface
@@ -42,15 +39,16 @@ BLUE = (200, 255, 255)
 win_width = 700
 win_height = 500
 window = display.set_mode((win_width, win_height))
+window.fill(BLUE)
 
 #create sprites (paddle and balls)
-paddleA = "paddle_blue.png"
-paddleB = "paddle_red.png"
+paddleA_img = "paddle_blue.png"
+paddleB_img = "paddle_red.png"
 ball_img = "tennis_ball.png"
 
-paddleLeft = Paddle (paddleA, 30, 200, 4, 50, 150)
-paddleRight = Paddle (paddleB, 620, 200, 4, 50, 150)
-ball = GameSprite(ball_img, 300, 200, 4, 50, 50)
+paddleLeft = Paddle (paddleA_img, 20, 200, 30, 150, 150)
+paddleRight = Paddle (paddleB_img, 650, 200, 30, 150, 150)
+ball = GameSprite(ball_img, 330, 200, 50, 50, 50)
 
 
 #game loop
@@ -59,6 +57,15 @@ finish = False
 clock = time.Clock()
 FPS = 60
 
+#fonts
+font.init()
+font = font.Font(None, 35)
+lose1 = font.render('PLAYER 1 LOSE!', True, (180, 0, 0))
+lose2 = font.render('PLAYER 2 LOSE!', True, (180, 0, 0))
+
+speed_x = 3
+speed_y = 3
+
 while game:
     for e in event.get():
         if e.type ==QUIT:
@@ -66,6 +73,12 @@ while game:
 
     if finish != True:
         window.fill (BLUE)
+        paddleLeft.update_left()
+        paddleRight.update_right()
+
+        ball.rect.x += speed_x
+        ball.rect.x += speed_y
+
         paddleLeft.reset()
         paddleRight.reset()
         ball.reset()
